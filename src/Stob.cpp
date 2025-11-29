e141d932fa691ac8662074b2553902397efc583c
SianyehGyan <149583525+SianyehGyan@users.noreply.github.com>
2025-11-29 11:11:05 +0800

    Update Stob.cpp
    Updated collision detection to use Vector3 for bullet position and added color settings for different parts.

--- src/Stob.cpp
+++ src/Stob.cpp
@@ -2,24 +2,36 @@
 #include "Stob.h"
 #include <cmath>
 #include <gl/glut.h>
+#include <iostream>
+#define disp(x) std::cout << x << std::endl;
 
-bool Stob::testCollision(float px, float py, float pz, float rBullet) {
-    float dx = px - x;
-    float dz = pz - z;
+bool Stob::testCollision(Vector3 posBullet, float rBullet) {
+    float dx = posBullet.x - x;
+    float dz = posBullet.z - z;
     float distXZ = sqrtf(dx * dx + dz * dz);
-    if(distXZ <= (radius + rBullet) && py >= y && py <= (y + h)) {
+    if(distXZ <= (radius + rBullet) && posBullet.y >= y && posBullet.y <= (y + h)) {
         return true;
     }
     return false;
 }
 
-void Stob::rebindTexture(enum TextureType type, char* bmpFile) {
+void Stob::rebindTexture(enum PartType type, char* bmpFile) {
     textures[type].rebind(bmpFile);
+}
+
+void Stob::setColor(enum PartType type, Color color) {
+    if(type == CAP) {
+        colorCap = color;
+    } else if(type == SIDE) {
+        colorSide = color;
+    }
 }
 
 void Stob::init() {
     slices = 20;
     textureEnabled = true;
+    colorSide = Color(0.2f, 0.8f, 0.2f);
+    colorCap = Color(0.8f, 0.2f, 0.8f);
 
     Texture textureside("resources/textures/WoodSide.bmp");
     textures.push_back(textureside);
@@ -45,7 +57,8 @@ void Stob::draw() {
         if(textureEnabled) glEnable(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, textures[1].ID);
         glBegin(GL_TRIANGLES);
-        glColor3f(1.0f, 1.0f, 1.0f);
+        if(textureEnabled) glColor3f(1.0f, 1.0f, 1.0f);
+        else glColor3f(colorCap.r, colorCap.g, colorCap.b);
         glNormal3f(0.0f, -1.0f, 0.0f);
         glTexCoord2f(0.5f, 0.5f);
         glVertex3f(0.0f, 0.0f, 0.0f);
@@ -56,7 +69,6 @@ void Stob::draw() {
         glTexCoord2f(0.5f + 0.5f * cosf(theta1), 0.5f + 0.5f * sinf(theta1));
         glVertex3f(cosf(theta1), 0.0f, sinf(theta1));
 
-        glColor3f(1.0f, 1.0f, 1.0f);
         glNormal3f(0.0f, 1.0f, 0.0f);
         glTexCoord2f(0.5f, 0.5f);
         glVertex3f(0.0f, 1.0f, 0.0f);
@@ -70,7 +82,8 @@ void Stob::draw() {
 
         glBindTexture(GL_TEXTURE_2D, textures[0].ID);
         glBegin(GL_QUADS);
-        glColor3f(1.0f, 1.0f, 1.0f);
+        if(textureEnabled) glColor3f(1.0f, 1.0f, 1.0f);
+        else glColor3f(colorSide.r, colorSide.g, colorSide.b);
         glNormal3f(cosf(theta1), 0.0f, sinf(theta1));
         glTexCoord2f((float)i / slices, 0.0f);
         glVertex3f(cosf(theta1), 0.0f, sinf(theta1));
