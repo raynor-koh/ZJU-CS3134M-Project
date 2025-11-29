15b3ec4cfb0e0125dd941acdb6b7d3d0635d3ba9
SianyehGyan <149583525+SianyehGyan@users.noreply.github.com>
2025-11-29 11:10:35 +0800

    Update Stob.h
    Updated Stob class to use Vector3 for position and added color functionality.

--- include/Stob.h
+++ include/Stob.h
@@ -2,24 +2,25 @@
 #include <GL/glew.h>
 #include <vector>
 #include "Texture.h"
+#include "GameObject.h"
 
 class Stob {
 public:
     float x, y, z, h, radius;
-    enum TextureType {SIDE=0, CAP=1};
+    enum PartType {SIDE=0, CAP=1};
     // Input: postion(centerX,bottomY,centerZ), height, radius
-    Stob(float ix, float iy, float iz, float ih, float iradius)
-        : x(ix), y(iy), z(iz), h(ih), radius(iradius) {init();}
-    Stob(float* posv, float ih, float iradius)
-        : x(posv[0]), y(posv[1]), z(posv[2]), h(ih), radius(iradius) {init();}
+    Stob(Vector3 pos, float ih, float iradius)
+        : x(pos.x), y(pos.y), z(pos.z), h(ih), radius(iradius) {init();}
     void draw();
     // Input: position and radius of bullet
-    bool testCollision(float px, float py, float pz, float rBullet);
+    bool testCollision(Vector3 posBullet, float rBullet);
     void setSlices(int s) { slices = s;}
-    void rebindTexture(enum TextureType type, char* bmpFile);
+    void rebindTexture(enum PartType type, char* bmpFile);
     void setTextureMode(bool enabled) { textureEnabled = enabled; }
+    void setColor(enum PartType type, Color color);
 private:
     int slices;
+    Color colorSide, colorCap;
     std::vector<Texture> textures;
     GLubyte image[imageheight][imageweight][1];
     bool textureEnabled;
